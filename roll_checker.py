c8699cbc8cf994e4ea04f200428dae7bb6710664
GabenRulez (PC Krosno) <wojtex1221@gmail.com>
2020-11-28 00:56:09 +0100

    Done: added random number generator (repaired for situations like "roll cow)"

--- roll_checker.py
+++ roll_checker.py
@@ -8,7 +8,10 @@ def check_for_roll(message):
     if(message.content == "losuj" or message.content == "roll" or message.content == "random" or message.content == "dice"):
         return throw_a_dice()
     if(message.content.startswith("losuj") or message.content.startswith("roll") or message.content.startswith("random")):
-        number = int(message.content.split()[1])
-        if(number <= 1):
-            return throw_a_dice()
-        return "I'm starting a random number generator (numbers from 1 to " + str(number) + "). I got <" + str(random.randrange(1, number)) + ">"
+        if(len(message.content.split()) <= 2):
+            if(message.content.split()[1].isnumeric()):
+                number = int(message.content.split()[1])
+                if(number <= 1):
+                    return throw_a_dice()
+                return "I'm starting a random number generator (numbers from 1 to " + str(number) + "). I got <" + str(random.randrange(1, number)) + ">"
+    return ""
